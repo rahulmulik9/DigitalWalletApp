330a997dc46c76004e84439e0282a29ac9a52714
Rahul Mulik <rahulmulik9@gmail.com>
2026-08-26 11:41:46 +0530

    Added Beneficiary Controller Repose Repository

---
+++ DigitalWallet/src/main/java/com/rahul/DigitalWallet/dto/beneficiary/BeneficiaryResponse.java
@@ -0,0 +1,12 @@
+package com.rahul.DigitalWallet.dto.beneficiary;
+
+import lombok.*;
+import java.time.LocalDateTime;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
+public class BeneficiaryResponse {
+    private Long id;
+    private Long beneficiaryWalletId;
+    private String nickname;
+    private LocalDateTime createdAt;
+}
