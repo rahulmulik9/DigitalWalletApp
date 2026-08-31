80a70ac96fe5ed814893e320e161c975762cc1ca
Rahul Mulik <rahul.mulik@prototechsolutions.com>
2026-08-31 11:32:41 +0530

    Added refresh scope so that every time when application yml file changes , no need to restart service

--- Phase8_Config_server/account-service/src/main/java/com/rahul/account_service/security/JwtTokenProvider.java
+++ Phase8_Config_server/account-service/src/main/java/com/rahul/account_service/security/JwtTokenProvider.java
@@ -4,11 +4,13 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.security.Keys;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.stereotype.Component;
 
 import javax.crypto.SecretKey;
 import java.util.Date;
 
+@RefreshScope
 @Component
 public class JwtTokenProvider {
 
