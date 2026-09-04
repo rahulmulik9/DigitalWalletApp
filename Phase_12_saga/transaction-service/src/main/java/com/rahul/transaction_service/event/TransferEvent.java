5e8bc5a99edffb370bcf1180fa09dc670b361f4c
Rahul Mulik <rahul.mulik@prototechsolutions.com>
2026-09-04 11:22:54 +0530

    Added new folder as phase 12

---
+++ Phase_12_saga/transaction-service/src/main/java/com/rahul/transaction_service/event/TransferEvent.java
@@ -0,0 +1,24 @@
+package com.rahul.transaction_service.event;
+
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+import java.time.Instant;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class TransferEvent {
+
+    private String eventType;      // "INITIATED" or "COMPLETED"
+    private Long transactionId;
+    private Long fromWalletId;
+    private Long toWalletId;
+    private BigDecimal amount;
+    private Instant timestamp;
+}
---
+++ Phase_12_saga/transaction-service/src/main/java/com/rahul/transaction_service/outbox/OutboxEventRepository.java
@@ -0,0 +1,10 @@
+package com.rahul.transaction_service.outbox;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface OutboxEventRepository extends JpaRepository<OutboxEvent, Long> {
+
+    List<OutboxEvent> findByStatusOrderByCreatedAtAsc(OutboxStatus status);
+}
